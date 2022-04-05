2543e914d971d62a4d6db9e328c32c3672deb693
Rahat-bin-abdullah-omi <rahatomi123@gmail.com>
2022-04-05 22:47:12 +0600

    blogs updated

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import './Blogs.css';
 
-const Blogs=()=> {
+const Blogs = () => {
   return (
     <div className='blogs'>
       <h2>
         1. What is contaxt api?
       </h2>
       <h3>
-        The Context API is a React structure that enables you to exchange unique details and assists in solving prop-drilling from all levels of your application. It is using for changing state. 
-        Redux is the best example of an context api.
+        The Context API is a React structure that enables you to exchange unique details and assists in solving prop-drilling from all levels of your application. It is using for changing state.Redux is the best example of an context api.
       </h3>
 
       <h2>
         2. What is symantic tag?
       </h2>
       <h3>
-        Semantic HTML elements are those that clearly describe their meaning in a human- and machine-readable way.
-        Now elements are header, footer, aside, artical ect.
+        Semantic HTML elements are those that clearly describe their meaning in a human- and machine-readable way.Now elements are header, footer,aside, article etc. Semantic HTML or semantic markup is HTML that introduces meaning to the web page rather than just presentation. For example, a paragraph tag indicates that the enclosed text is a paragraph. This is both semantic and presentational because people know what paragraphs are, and browsers know how to display them.
       </h3>
     </div>
   )
--- src/components/NoFound/NoFound.js
+++ src/components/NoFound/NoFound.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 function NoFound() {
   return (
-          <div>
-        <h1> Sorry Nothing Found!!!!  </h1>         
-         </div>
+    <div>
+      <h1> 404 </h1>
+      <h1> Sorry Nothing Found!!!!  </h1>
+    </div>
   )
 }
 
